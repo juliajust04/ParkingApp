4ee542f006050076d300a47fdcab53af568aeda1
Julia <julia.just@edu.uekat.pl>
2025-09-02 16:57:55 +0200

    fix layout

--- app/src/main/java/com/example/parkingapp/MainActivity.kt
+++ app/src/main/java/com/example/parkingapp/MainActivity.kt
@@ -1,33 +1,20 @@
 package com.example.parkingapp
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.parkingapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
+        setContentView(R.layout.activity_main)
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        val navView = findViewById<BottomNavigationView>(R.id.nav_view)
+
         navView.setupWithNavController(navController)
     }
 }
